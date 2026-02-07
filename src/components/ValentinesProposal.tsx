b20baa8155bfb545bb4b3e51e7b6d83aad8ee09a
merlijnhoffman <merlijnhoffman@gmail.com>
2026-02-07 19:23:50 +0700

    Update ValentinesProposal.tsx

--- src/components/ValentinesProposal.tsx
+++ src/components/ValentinesProposal.tsx
@@ -132,7 +132,7 @@ export default function ValentinesProposal() {
             <h2
               className={`text-5xl font-semibold mb-8 ${playfairDisplay.className}`}
             >
-              Will you be my Valentine?
+              Will you be my Valentine Lola?
             </h2>
             <Image
               src="/sad_hamster.png"
@@ -174,7 +174,7 @@ export default function ValentinesProposal() {
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
           >
-            Thank you for accepting, I love you! 💕
+            Thank you for accepting, I love you (more)! 💕
             <p className="text-sm mt-4">For more information, write me!!! 💌</p>
             <Image
               src="/hamster_jumping.gif"
